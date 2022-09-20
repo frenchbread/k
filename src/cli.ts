bdee986189854e19d0ac7ab9f1263ee7bfb9d156
Damir Mustafin <damir.mustafin@protonmail.ch>
2022-09-20 10:47:12 +0300

    add help text

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,19 @@
+#!/usr/bin/env node
+'use strict'
 import clear from 'clear'
+import meow from 'meow'
 
 import K from './'
 
 const k = new K()
+
+meow(`
+	Usage
+	  ❯ k
+
+	Options
+	  --help, -h  Show help
+`)
 
 clear()
 k.init()
